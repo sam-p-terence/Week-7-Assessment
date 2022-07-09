bdc107c2b47791b827c55fe071e72da5bff99b33
Terence Sam <sam.p.terence@gmail.com>
2022-07-09 14:45:47 -0600

    initial init

---
+++ code.js
@@ -0,0 +1,62 @@
+
+
+//Add to Sum
+
+// let array = []
+// let array = [1]
+// let array = [1, 2, 3]
+// let array = [1, 2, 3, -1]
+let array = [10, -10]
+
+
+let value = false;
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (i !== j) {
+      if (array[i] + array[j] === 0) {
+        value = true;
+      } 
+    }
+  }
+}
+// console.log(value)
+
+
+
+
+
+//Is the sentence a Pangram
+
+function isPangram(string){
+    let strArr = string.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    for (let i = 0; i < alphabet.length; i++) {
+      if(strArr.indexOf(alphabet[i]) < 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+//   console.log(isPangram("The quick brown fox jumps over the lazy dog!"))
+//   console.log(isPangram("I like cats, but not mice"))
+
+
+
+
+//Find the longest word
+
+len = ["Hello", "University", "Presidential"]
+
+function findLongestWord(arr) {
+    let len = 0
+    for (let i = 0; i < arr.length; i++) {
+      if( arr[i].length > len) {
+        len = arr[i].length
+      }
+    }
+    return len
+  }
+
+  console.log(findLongestWord(len))
